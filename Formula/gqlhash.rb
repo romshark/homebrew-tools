6e47686eb149f9f51d3a5d36ecc2a60bc46cae12
Roman Sharkov <roman.scharkov@gmail.com>
2024-11-04 01:56:54 +0100

    Upgrade gqlhash to v1.2.1

--- Formula/gqlhash.rb
+++ Formula/gqlhash.rb
@@ -1,7 +1,7 @@
 class Gqlhash < Formula
   desc "GraphQL executable document hasher that ignores formatting diffs and comments"
   homepage "https://github.com/romshark/gqlhash"
-  url "https://github.com/romshark/gqlhash/archive/refs/tags/v1.2.0.tar.gz"
+  url "https://github.com/romshark/gqlhash/archive/refs/tags/v1.2.1.tar.gz"
   sha256 "d3d9b103b85549845705425eb18af3cb917fb0aa69387d54a65377cfd4aacf85"
   license "MIT"
 
@@ -13,6 +13,6 @@ class Gqlhash < Formula
 
   test do
     output = shell_output("#{bin}/gqlhash -version")
-    assert_match "gqlhash v1.2.0", output
+    assert_match "gqlhash v1.2.1", output
   end
 end
